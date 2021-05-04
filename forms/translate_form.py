9095f3394f73c134af418600d239f4454e3bb79a
SUPERustam <498rustam@gmail.com>
2021-05-04 17:17:28 +0300

    blackout

--- forms/translate_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-from sqlalchemy import orm
-
-
-class TranslateForm(FlaskForm):
-    content = TextAreaField("Русский язык", validators=[DataRequired()])
-    submit = SubmitField("Перевод")
